651003ccbdbef04a877bdfba4878f91fcd45c03d
Prudhvi Nelaturi <prudhvinelaturi29@gmail.com>
2024-06-07 23:52:09 -0400

    Updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav
+      className={`
+        ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-flex items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive('');
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-1 object-contain" />
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
